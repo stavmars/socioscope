1064ce8d95e77cc56e6ff25d6836fdf5f6bb2724
Asteris <asteris.gian@gmail.com>
2019-07-04 12:33:48 +0300

    Added footer translated fields

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -1,6 +1,7 @@
 import './footer.scss';
 
 import React from 'react';
+import { translate } from 'react-jhipster';
 import { Card, CardBody, Col, Row } from 'reactstrap';
 import { Grid, Image, List } from 'semantic-ui-react';
 
@@ -10,15 +11,15 @@ const Footer = props => (
       <Grid.Row>
         <Grid.Column width={8}>
           <List id="app-footer-links" horizontal bulleted>
-            <List.Item as="a">ΣΧΕΤΙΚΑ</List.Item>
-            <List.Item as="a">ΟΡΟΙ ΧΡΗΣΗΣ</List.Item>
-            <List.Item as="a">ΠΟΛΙΤΙΚΗ ΑΠΟΡΡΗΤΟΥ</List.Item>
-            <List.Item as="a">DEVELOPERS</List.Item>
+            <List.Item as="a">{translate('footer.about')}</List.Item>
+            <List.Item as="a">{translate('footer.terms')}</List.Item>
+            <List.Item as="a">{translate('footer.policy')}</List.Item>
+            <List.Item as="a">{translate('footer.developers')}</List.Item>
           </List>
           <Image src="/content/images/Assets/Logo-white-footer.png" />
         </Grid.Column>
         <Grid.Column width={2}>
-          <div style={{ height: 40 }}>Μία συνεργασία των</div>
+          <div style={{ height: 40 }}>{translate('footer.collaboration')}</div>
           <Image.Group>
             <Image src="/content/images/Assets/ekke.png" style={{ marginRight: 20 }} />
             <Image src="/content/images/Assets/athena.png" style={{ marginLeft: 20 }} />
