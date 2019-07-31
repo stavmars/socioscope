f745df883f485deca38f881c3c5eafcf3de8f733
Asteris <asteris.gian@gmail.com>
2019-07-31 16:42:27 +0300

    Added chartColors arra.

--- src/main/webapp/app/config/constants.ts
+++ src/main/webapp/app/config/constants.ts
@@ -27,3 +27,26 @@ export const accentColors = {
   'color-scheme-2': '#2f7cff',
   'color-scheme-3': '#ff5d39'
 };
+
+export const chartColors = [
+  '#e6194B',
+  '#3cb44b',
+  '#ffe119',
+  '#4363d8',
+  '#f58231',
+  '#911eb4',
+  '#42d4f4',
+  '#f032e6',
+  '#bfef45',
+  '#fabebe',
+  '#469990',
+  '#e6beff',
+  '#9A6324',
+  '#fffac8',
+  '#800000',
+  '#aaffc3',
+  '#808000',
+  '#ffd8b1',
+  '#000075',
+  '#a9a9a9'
+];
