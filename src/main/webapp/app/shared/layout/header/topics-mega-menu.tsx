476b8c297a70b49aca3e11fda16c92e68bcf01ff
Asteris <asteris.gian@gmail.com>
2019-09-02 12:09:10 +0300

    Changed order of topics.

--- src/main/webapp/app/shared/layout/header/topics-mega-menu.tsx
+++ src/main/webapp/app/shared/layout/header/topics-mega-menu.tsx
@@ -20,7 +20,7 @@ export class TopicsMegaMenu extends React.Component<ITopicsMegaMenuProps> {
         <Header isFixed={false} />
         <div className="topics-mega-menu-list">
           <List selection verticalAlign="middle">
-            {_.at(dataSetsById, ['greek-election-results', 'deputies', 'adolescents', 'claims']).map((dataset: IDataSet) => (
+            {_.at(dataSetsById, ['adolescents', 'deputies', 'greek-election-results', 'claims']).map((dataset: IDataSet) => (
               <List.Item className={`topics-mega-menu-list-item ${dataset.colorScheme}`} as={Link} to={`/dataset/${dataset.id}`}>
                 <div className="topics-mega-menu-img">
                   <Image centered src={`/content/images/Assets/${dataset.id}.svg`} />
