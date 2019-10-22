ac7bbe5d81a2165794acf311c5290db4b716a353
Asteris <asteris.gian@gmail.com>
2019-10-22 11:57:50 +0300

    Graph/Map options appear only with geographic dimensions.

--- src/main/webapp/app/modules/dataset-page/vis-toolbar.tsx
+++ src/main/webapp/app/modules/dataset-page/vis-toolbar.tsx
@@ -114,19 +114,21 @@ export class VisToolbar extends React.Component<IVisToolBarProp, IVisToolBarStat
                       </Form>
                     </Menu.Item>
                   )}
-                <Menu.Item
-                  as={NavLink}
-                  to={'?' + urlEncodeVisOptions({ visType: 'chart', subType: 'column', seriesOptions })}
-                  active={visType === 'chart'}
-                  style={{ marginRight: '50px' }}
-                >
-                  {visType === 'chart' ? (
-                    <Image src={`/content/images/Assets/Chart-${colorScheme}.svg`} style={{ marginRight: '20px' }} />
-                  ) : (
-                    <Image src={`/content/images/Assets/Chart.svg`} style={{ marginRight: '20px' }} />
-                  )}
-                  {translate('socioscopeApp.dataSet.visualization.graph')}
-                </Menu.Item>
+                {_.find(dataset.dimensions as IDimension[], obj => obj.type === 'geographic-area') && (
+                  <Menu.Item
+                    as={NavLink}
+                    to={'?' + urlEncodeVisOptions({ visType: 'chart', subType: 'column', seriesOptions })}
+                    active={visType === 'chart'}
+                    style={{ marginRight: '50px' }}
+                  >
+                    {visType === 'chart' ? (
+                      <Image src={`/content/images/Assets/Chart-${colorScheme}.svg`} style={{ marginRight: '20px' }} />
+                    ) : (
+                      <Image src={`/content/images/Assets/Chart.svg`} style={{ marginRight: '20px' }} />
+                    )}
+                    {translate('socioscopeApp.dataSet.visualization.graph')}
+                  </Menu.Item>
+                )}
                 {_.find(dataset.dimensions as IDimension[], obj => obj.type === 'geographic-area') && (
                   <Menu.Item
                     as={NavLink}
