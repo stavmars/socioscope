ca1f529c271bf16014bc15508ff78b4e008492dc
Asteris <asteris.gian@gmail.com>
2019-10-22 12:07:46 +0300

    Code polish.

--- src/main/webapp/app/modules/dataset-page/vis-toolbar.tsx
+++ src/main/webapp/app/modules/dataset-page/vis-toolbar.tsx
@@ -115,34 +115,34 @@ export class VisToolbar extends React.Component<IVisToolBarProp, IVisToolBarStat
                     </Menu.Item>
                   )}
                 {_.find(dataset.dimensions as IDimension[], obj => obj.type === 'geographic-area') && (
-                  <Menu.Item
-                    as={NavLink}
-                    to={'?' + urlEncodeVisOptions({ visType: 'chart', subType: 'column', seriesOptions })}
-                    active={visType === 'chart'}
-                    style={{ marginRight: '50px' }}
-                  >
-                    {visType === 'chart' ? (
-                      <Image src={`/content/images/Assets/Chart-${colorScheme}.svg`} style={{ marginRight: '20px' }} />
-                    ) : (
-                      <Image src={`/content/images/Assets/Chart.svg`} style={{ marginRight: '20px' }} />
-                    )}
-                    {translate('socioscopeApp.dataSet.visualization.graph')}
-                  </Menu.Item>
-                )}
-                {_.find(dataset.dimensions as IDimension[], obj => obj.type === 'geographic-area') && (
-                  <Menu.Item
-                    as={NavLink}
-                    to={'?' + urlEncodeVisOptions({ visType: 'map', seriesOptions: dataset.defaultOptions })}
-                    active={visType === 'map'}
-                    style={{ marginRight: '50px', a: { pointerEvents: 'none' } }}
-                  >
-                    {visType === 'map' ? (
-                      <Image src={`/content/images/Assets/Map-${colorScheme}.svg`} style={{ marginRight: '20px' }} />
-                    ) : (
-                      <Image src={`/content/images/Assets/Map.svg`} style={{ marginRight: '20px' }} />
-                    )}
-                    {translate('socioscopeApp.dataSet.visualization.map')}
-                  </Menu.Item>
+                  <div style={{ display: 'inherit' }}>
+                    <Menu.Item
+                      as={NavLink}
+                      to={'?' + urlEncodeVisOptions({ visType: 'chart', subType: 'column', seriesOptions })}
+                      active={visType === 'chart'}
+                      style={{ marginRight: '50px' }}
+                    >
+                      {visType === 'chart' ? (
+                        <Image src={`/content/images/Assets/Chart-${colorScheme}.svg`} style={{ marginRight: '20px' }} />
+                      ) : (
+                        <Image src={`/content/images/Assets/Chart.svg`} style={{ marginRight: '20px' }} />
+                      )}
+                      {translate('socioscopeApp.dataSet.visualization.graph')}
+                    </Menu.Item>
+                    <Menu.Item
+                      as={NavLink}
+                      to={'?' + urlEncodeVisOptions({ visType: 'map', seriesOptions: dataset.defaultOptions })}
+                      active={visType === 'map'}
+                      style={{ marginRight: '50px', a: { pointerEvents: 'none' } }}
+                    >
+                      {visType === 'map' ? (
+                        <Image src={`/content/images/Assets/Map-${colorScheme}.svg`} style={{ marginRight: '20px' }} />
+                      ) : (
+                        <Image src={`/content/images/Assets/Map.svg`} style={{ marginRight: '20px' }} />
+                      )}
+                      {translate('socioscopeApp.dataSet.visualization.map')}
+                    </Menu.Item>
+                  </div>
                 )}
               </Menu>
             </Grid.Column>
