f477a3c4fb91f05fb5feeb0193a4c843d3d3837a
Asteris <asteris.gian@gmail.com>
2019-11-12 13:02:41 +0200

    Styling configured on all devices.

--- src/main/webapp/app/modules/blog/blog-page.tsx
+++ src/main/webapp/app/modules/blog/blog-page.tsx
@@ -18,15 +18,13 @@ export class BlogPage extends React.Component<DispatchProps> {
       <div className="blog-page">
         <h1 className="blog-page-title">Πρόσκληση παρουσίασης του νέου Socioscope.gr</h1>
         <div className="blog-page-divider" />
-        <Grid style={{ marginLeft: '20%' }}>
+        <Grid className="blog-page-grid">
           <Grid.Row className="info-row">
-            <Grid.Column computer={1}>
+            <Grid.Column className="blog-page-col-1">
               <Image className="blog-page-image" src="/content/images/Assets/Calendar.png" />
+              <span className="blog-page-question">Πότε;</span>
             </Grid.Column>
-            <Grid.Column computer={1} className="blog-page-question" verticalAlign="middle">
-              Πότε;
-            </Grid.Column>
-            <Grid.Column computer={3} className="blog-page-answer" verticalAlign="middle">
+            <Grid.Column computer={3} mobile={10} className="blog-page-answer" verticalAlign="middle">
               Τρίτη 19 Νοεμβρίου 2019
             </Grid.Column>
           </Grid.Row>
@@ -36,24 +34,20 @@ export class BlogPage extends React.Component<DispatchProps> {
             href="https://www.google.com/maps/place/six+d.o.g.s/@37.9774684,23.7268116,15z/data=!4m2!3m1!1s0x0:0x1064e7b168cee196?sa=X&ved=2ahUKEwjerP6JltjlAhUyxaYKHWNmAEoQ_BIwCnoECA8QCA"
             target="_blank"
           >
-            <Grid.Column computer={1}>
+            <Grid.Column className="blog-page-col-1">
               <Image className="blog-page-image" src="/content/images/Assets/Pin.png" />
+              <span className="blog-page-question">Πού;</span>
             </Grid.Column>
-            <Grid.Column computer={1} className="blog-page-question" verticalAlign="middle">
-              Πού;
-            </Grid.Column>
-            <Grid.Column computer={3} className="blog-page-answer" verticalAlign="middle">
+            <Grid.Column computer={3} mobile={10} className="blog-page-answer" verticalAlign="middle">
               s.i.x. dogs (Στο Gig Space) Αμβρωτίου 6-8, Αθήνα 105 51
             </Grid.Column>
           </Grid.Row>
           <Grid.Row className="info-row">
-            <Grid.Column computer={1}>
+            <Grid.Column className="blog-page-col-1">
               <Image className="blog-page-image" src="/content/images/Assets/Clock.png" />
+              <span className="blog-page-question">Ώρα;</span>
             </Grid.Column>
-            <Grid.Column computer={1} className="blog-page-question" verticalAlign="middle">
-              Ώρα;
-            </Grid.Column>
-            <Grid.Column computer={3} className="blog-page-answer" verticalAlign="middle">
+            <Grid.Column computer={3} mobile={10} className="blog-page-answer" verticalAlign="middle">
               14:00 - 16:00
             </Grid.Column>
           </Grid.Row>
@@ -81,13 +75,32 @@ export class BlogPage extends React.Component<DispatchProps> {
           </p>
           <p>Σας περιμένουμε</p>
           <p>
-            <span>Μανίνα Κακεπάκη</span> Ερευνήτρια ΙΚΕ-ΕΚΚΕ <span>| mkapekaki@ekke.gr</span>
+            <span>Μανίνα Κακεπάκη</span> Ερευνήτρια ΙΚΕ-ΕΚΚΕ{' '}
+            <span>
+              |{' '}
+              <a href="mailto:mkapekaki@ekke.gr" target="_blank">
+                mkapekaki@ekke.gr
+              </a>
+            </span>
           </p>
           <p>
-            <span>Κατερίνα Ηλιού</span> Ερευνήτρια ΙΚΕ-ΕΚΚΕ <span>| mkapekaki@ekke.gr</span>
+            <span>Κατερίνα Ηλιού</span> Ερευνήτρια ΙΚΕ-ΕΚΚΕ{' '}
+            <span>
+              |{' '}
+              <a href="mailto:kiliou@ekke.gr" target="_blank">
+                kiliou@ekke.gr
+              </a>
+            </span>
           </p>
-          <Button className="blog-page-register">Δηλώστε Συμμετοχή</Button>
-          <Menu text borderless style={{ backgroundColor: 'transparent', borderStyle: 'none', marginLeft: '40%' }}>
+          <Button
+            className="blog-page-register"
+            as="a"
+            href="https://docs.google.com/forms/d/e/1FAIpQLScluDhIE8v956Kuvr0iGceLwSV4nvRUAEPc2ru60HKY6POIng/viewform"
+            target="_blank"
+          >
+            Δηλώστε Συμμετοχή
+          </Button>
+          <Menu className="blog-page-contact" text borderless style={{ backgroundColor: 'transparent', borderStyle: 'none' }}>
             <Menu.Item as="a" href="https://www.facebook.com/socioscope/" target="_blank">
               <Icon name="facebook official" />
               Socioscope.gr
