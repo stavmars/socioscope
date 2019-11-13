7e916ecf324fda8f060a116bdaaae5c90320348c
Asteris <asteris.gian@gmail.com>
2019-11-13 17:39:19 +0200

    Added fb icon on mobile header.

--- src/main/webapp/app/shared/layout/header/header.tsx
+++ src/main/webapp/app/shared/layout/header/header.tsx
@@ -63,7 +63,10 @@ export class Header extends React.Component<IHeaderProps, {}> {
               <Menu.Item position="left" header as="a" href="/" style={{ padding: '18px 0 17px 15px' }}>
                 <Image src={`/content/images/Assets/Logo-${color}.svg`} alt="Socioscope Logo" style={{ height: '35', width: '144px' }} />
               </Menu.Item>
-              <Menu.Item onClick={this.handleLocaleChange} style={{ padding: '14px 0 13px 60px' }}>
+              <Menu.Item as="a" href="https://www.facebook.com/socioscope/" target="_blank" style={{ padding: '14px 10px 13px 60px' }}>
+                <Image src={facebookIcon} style={{ width: '27px', height: '27px' }} />
+              </Menu.Item>
+              <Menu.Item onClick={this.handleLocaleChange} style={{ padding: '14px 0 13px 0' }}>
                 <Image src={localeIcon} alt="language switcher" style={{ width: '27px', height: '27px' }} />
               </Menu.Item>
               <Menu.Item onClick={this.props.toggleMobileMenu} style={{ padding: '21px 20px 21px 18px' }}>
